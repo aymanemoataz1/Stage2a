61a966accb37fe265abe3e85927d71365026e14a
aymanemoataz1 <56265854+aymanemoataz1@users.noreply.github.com>
2020-08-14 15:14:04 +0200

    Update text recognition.py

--- code/text recognition.py
+++ code/text recognition.py
@@ -1,13 +1,11 @@
-ex="ADROIT\n\nSOLAR + ENERGY + DESIGN\n\n \n\n1135 GARNET Ave, STE 32\nSAN DieGo, CA 92109\n\nUtaw WHITE (T) 858.483.3568 x16\na a x\nMECHANICAL ENGINEER (C) 858.692.6261\nPE, LEED AP (F) 858.492.7772\nUTAW@ADROITSOLAR.COM CA Lic 488255 Hi Lic 29995\n\nWWW.ADROITSOLAR.COM"
-l=[]
-ex1=str()
-for i in ex:
+import cv2
+from PIL import Image
+import pytesseract
 
-    if i!="\n":
-        ex1+=i        
-    else:
-        if ex1!=str():
-           l.append(ex1)
-           ex1=str()
+
+pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+im=Image.open('C:\\Users\\Aymane\\Desktop\\stage data\\programme\\data\\card5.png')
+ex=pytesseract.image_to_string(im)
         
  
